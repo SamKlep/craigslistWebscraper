e12704660bd165d82dd38610efd12d784e2fa5a3
Samuel Klepper <55067844+SamKlep@users.noreply.github.com>
2020-04-17 11:24:22 -0500

    denoised results with .trim() and .replace()

--- index.js
+++ index.js
@@ -23,11 +23,16 @@ async function main() {
     const results = $(".result-info")
         .map((index, element) => {
             const titleElement = $(element).find(".result-title");
-            const timeelement = $(element).find(".result-date");
+            const timeElement = $(element).find(".result-date");
+            const hoodElement = $(element).find(".result-hood");
             const title = $(titleElement).text();
             const url = $(titleElement).attr("href");
-            const datePosted = new Date($(timeelement).attr("datetime"));
-            return { title, url, datePosted };
+            const datePosted = new Date($(timeElement).attr("datetime"));
+            const hood = $(hoodElement)
+                .text()
+                .trim().replace("(","")
+                .replace(")","");
+            return { title, url, datePosted, hood };
         })
         .get();
     console.log(results);
